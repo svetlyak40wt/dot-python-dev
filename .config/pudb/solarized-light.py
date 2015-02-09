8200585a92b6124e0691b5e2a7c1f96b7c2923c8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-02-09 14:12:21 +0000

    Add config and light solarized theme for pudb.

---
+++ .config/pudb/solarized-light.py
@@ -0,0 +1,120 @@
+# coding: utf-8
+
+# Slightly modified "solarized" theme for pudb
+# ============================================
+#
+# Supported 16 color values:
+#   'h0' (color number 0) through 'h15' (color number 15)
+#    or
+#   'default' (use the terminal's default foreground),
+#   'black', 'dark red', 'light green', 'brown', 'dark blue',
+#   'dark magenta', 'dark cyan', 'light gray', 'dark gray',
+#   'light red', 'light green', 'yellow', 'light blue',
+#   'light magenta', 'light cyan', 'white'
+#
+# Supported 256 color values:
+#   'h0' (color number 0) through 'h255' (color number 255)
+#
+# 256 color chart: http://en.wikipedia.org/wiki/File:Xterm_color_chart.png
+#
+# Based on XCode's midnight theme
+# Looks best in iTerm2 with light solarized color theme.
+#
+# ATTENTION: Be sure to set 'raw' terminal driver in pudb's settings
+#            to enable 256 color support!
+
+palette.update({
+    # UI
+    "header": ("black", "light blue", "standout"),
+    "focused sidebar": ("yellow", "light blue", "standout"),
+    "group head": ("black", "light blue"),
+    "background": ("black", "light blue"),
+    "label": ("black", "light blue"),
+    "value": ("white", "dark blue"),
+    "fixed value": ("black", "light blue"),
+
+    "variables": ("light blue", "default"),
+
+    "var label": ("dark blue", "default"),
+    "var value": ("light blue", "default"),
+
+    "focused var label": ("white", "dark blue"),
+    "focused var value": ("black", "dark blue"),
+
+    "highlighted var label": ("white", "light green"),
+    "highlighted var value": ("white", "light green"),
+    "focused highlighted var label": ("white", "light green"),
+    "focused highlighted var value": ("white", "light green"),
+
+    "stack": ("light blue", "default"),
+
+    "frame name": ("dark blue", "default"),
+    "frame class": ("light blue", "default"),
+    "frame location": ("light green", "default"),
+
+    "focused frame name": ("white", "dark blue"),
+    "focused frame class": ("black", "dark blue"),
+    "focused frame location": ("dark gray", "dark blue"),
+
+    "focused current frame name": ("white", "light green"),
+    "focused current frame class": ("black", "light green"),
+    "focused current frame location": ("dark gray", "light green"),
+
+    "current frame name": ("white", "light green"),
+    "current frame class": ("black", "light green"),
+    "current frame location": ("dark gray", "light green"),
+
+    # breakpoints
+    "breakpoint": ("light blue", "default"),
+    "focused breakpoint": ("white", "light green"),
+    "current breakpoint": ("white", "dark blue"),
+    "focused current breakpoint": ("white", "dark blue"),
+
+    # source
+    "breakpoint source": ("light blue", "black"),
+    "current breakpoint source": ("black", "light green"),
+    "breakpoint focused source": ("dark gray", "dark blue"),
+    "current breakpoint focused source": ("black", "light green"),
+    "breakpoint marker": ("dark red", "default"),
+
+    "search box": ("default", "default"),
+
+    "source": ("light blue", "default"),
+    "current source": ("h231", "h211"),
+    "current focused source": ("h231", "h140"),
+
+    "focused source": ("dark gray", "h69"),
+
+    "current highlighted source": ("black", "dark cyan"),
+    "highlighted source": ("light blue", "black"),
+
+    "line number": ("light blue", "default"),
+    "keyword": ("dark green", "default"),
+    "name": ("light blue", "default"),
+    "literal": ("dark cyan", "default"),
+    "string": ("dark cyan", "default"),
+    "doublestring": ("dark cyan", "default"),
+    "singlestring": ("light blue", "default"),
+    "docstring": ("dark cyan", "default"),
+    "backtick": ("light green", "default"),
+    "punctuation": ("light blue", "default"),
+    "comment": ("light green", "default"),
+    "classname": ("dark blue", "default"),
+    "funcname": ("dark blue", "default"),
+
+    # shell
+
+    "command line edit": ("light blue", "default"),
+    "command line prompt": ("light blue", "default"),
+
+    "command line output": ("light blue", "default"),
+    "command line input": ("light blue", "default"),
+    "command line error": ("dark red", "default"),
+
+    "focused command line output": ("black", "light green"),
+    "focused command line input": ("black", "light green"),
+    "focused command line error": ("dark red", "light blue"),
+
+    "command line clear button": ("light blue", "default"),
+    "command line focused button": ("black", "light blue"),
+})
---
+++ ChangeLog.md
@@ -0,0 +1,7 @@
+0.1.0
+=====
+
+* Bind "C-c g" to magit-status.
+* Add config and light solarized theme for [pudb][]. Color theme is compatible with iTerm2's light solarized theme.
+
+[pudb]: https://pypi.python.org/pypi/pudb
