4ae8fb21be50a719fef22ad37c394d2e95d37238
Alexander Artemenko <art@yandex-team.ru>
2018-03-28 12:41:48 +0000

    Поправлены настройки pudb и pdbrc.

--- .pdbrc.py
+++ .pdbrc.py
@@ -8,6 +8,10 @@ class Config(DefaultConfig):
     filename_color = Color.darkgreen
     line_number_color = Color.brown
     current_line_color = int(Color.darkred)
+    encoding = 'utf-8'
+    # Это нужно из-за того, что при подтсветке кода иногда ломаются докстринги
+    # и комменты на русском.
+    use_pygments = False
 
     def setup(self, pdb):
         Pdb = pdb.__class__
