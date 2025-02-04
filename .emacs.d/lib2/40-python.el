89959f2b6bf247c58fcea64d4ee6ab0cf8c5055a
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-02-04 11:45:55 +0000

    Setup flycheck and importmagic

--- .emacs.d/lib2/40-python.el
+++ .emacs.d/lib2/40-python.el
@@ -22,11 +22,56 @@
   :commands lsp)
 
 
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode)
+  :config
+  (keymap-set global-map "M-p" 'flycheck-previous-error)
+  (keymap-set global-map "M-n" 'flycheck-next-error))
+
+
+;; This plugin works together with Powerline-mode,
+;; coloring file name in the mode line into different
+;; colors depending on number errors or warnings
+(use-package flycheck-color-mode-line
+  :ensure t
+  :after flycheck
+  :hook flycheck-mode)
+
+
+(defun 40wt/pytest-one (&optional arg)
+  (interactive "P")
+  (if arg
+      (pytest-pdb-one)
+    (pytest-one)))
+
+
 (use-package pytest
   :custom
   ((pytest-project-root-files '("service.yaml" "setup.py" ".hg" ".git")))
   :config
-  (keymap-set python-mode-map "C-t o" 'pytest-one)
+  (keymap-set python-mode-map "C-t o" '40wt/pytest-one)
   (keymap-set python-mode-map "C-t a" 'pytest-again)
   (keymap-set python-mode-map "C-t A" 'pytest-all)
   (keymap-set python-mode-map "C-t m" 'pytest-module))
+
+
+(defun 40wt/pytest-again-with-sql-regen ()
+  ;; Changes a command like this
+  ;; cd '/codenv/arcadia/taxi/backend-py3/services/dmp-sched/' && /codenv/arcadia/taxi/backend-py3/runtests -x -s ''/codenv/arcadia/taxi/backend-py3/services/dmp-sched/test_dmp_sched/cron/test_job_manager.py::test_scheduling_attempt''
+
+  (if-let* ((last-command (gethash (pytest-get-temp-buffer-name) pytest-last-commands))
+            (command (replace-regexp-in-string "&&"
+                                               "&& /codenv/arcadia/ya tool tt gen -p services/dmp-sched --plugins sqlt postgres_queries &&"
+                                               last-command)))
+      (pytest-start-command command)
+    (error "Pytest has not run before")))
+
+
+;; https://github.com/emacsmirror/importmagic
+;; To make it work, do pip install importmagic epc
+(use-package importmagic
+  :ensure t
+  :hook python-mode
+  :config
+  (keymap-set importmagic-mode-map "C-c C-l" 'importmagic-fix-symbol-at-point))
